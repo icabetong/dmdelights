76f74f8119bad7db3865b64e5324bcc498719150
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2022-01-19 20:52:48 +0800

    login

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -1,3 +1,4 @@
+import 'package:dm_delights/core/supabase.dart';
 import 'package:dm_delights/shared/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/translations.dart';
@@ -10,6 +11,43 @@ class SignUpFragment extends StatefulWidget {
 }
 
 class _SignUpFragmentState extends State<SignUpFragment> {
+  bool _isLoading = false;
+  late final TextEditingController _emailController;
+  late final TextEditingController _passwordController;
+  late final TextEditingController _repasswordController;
+
+  Future<void> _signUp() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    final response = await Backend.instance.auth.signUp(email, password);
+
+    final error = response.error;
+    if (error != null) {
+      debugPrint(error.toString());
+    } else {
+      _emailController.clear();
+      _passwordController.clear();
+      _repasswordController.clear();
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    _repasswordController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -23,6 +61,7 @@ class _SignUpFragmentState extends State<SignUpFragment> {
                   border: const OutlineInputBorder(),
                   labelText: Translations.of(context)!.field_email,
                 ),
+                controller: _emailController,
               ),
               SizedBox(height: ThemeComponents.largeSpacing),
               TextFormField(
@@ -30,6 +69,7 @@ class _SignUpFragmentState extends State<SignUpFragment> {
                   border: const OutlineInputBorder(),
                   labelText: Translations.of(context)!.field_password,
                 ),
+                controller: _passwordController,
               ),
               SizedBox(height: ThemeComponents.largeSpacing),
               TextFormField(
@@ -37,14 +77,17 @@ class _SignUpFragmentState extends State<SignUpFragment> {
                   border: const OutlineInputBorder(),
                   labelText: Translations.of(context)!.field_retype_password,
                 ),
+                controller: _repasswordController,
               ),
               SizedBox(height: ThemeComponents.defaultSpacing),
               SizedBox(
                 width: double.infinity,
                 height: 48,
                 child: ElevatedButton(
-                  child: Text(Translations.of(context)!.button_signup),
-                  onPressed: () {},
+                  child: Text(_isLoading
+                      ? Translations.of(context)!.feedback_authenticating
+                      : Translations.of(context)!.button_signup),
+                  onPressed: _isLoading ? null : _signUp,
                 ),
               )
             ],
